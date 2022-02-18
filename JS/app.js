54168e0717127025f381ba6502447fea2fec6f5f
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-02-18 21:00:56 +0600

    saving function

--- JS/app.js
+++ JS/app.js
@@ -9,6 +9,7 @@ function allInputValue(inputId) {
 
 // ---- Calculate for Total Expenses & Net Balance ----
 function calculation(foodAmount, rentAmount, clothesAmount, inputAmount) {
+
     // ---- Get Expenses & Balance innerText Id
     const expensesResult = document.getElementById("total-expenses");
     const balanceResult = document.getElementById("total-balance");
@@ -41,4 +42,35 @@ document.getElementById("calculate-button").addEventListener("click", function (
 
     // ---- Calculate Function
     calculation(foodAmount, rentAmount, clothesAmount, inputAmount);
+});
+
+// ---- Click Event for Save Button
+document.getElementById("save-button").addEventListener("click", function () {
+    const savingAmount = allInputValue("saving");
+    const savingResult = document.getElementById("saving-balance");
+
+    const balanceResult = document.getElementById("total-balance");
+    const balanceValue = parseFloat(balanceResult.innerText);
+
+    const remainingResult = document.getElementById("remaining-balance");
+
+    if (savingAmount > 0 && savingAmount <= 100) {
+        if (balanceValue > 0) {
+            const savingValue = (savingAmount * balanceValue) / 100;
+            savingResult.innerText = savingValue.toFixed(4);
+
+            const netBalance = balanceValue - savingValue;
+            remainingResult.innerText = netBalance.toFixed(4);
+        }
+        else {
+            alert("Balance is too low for saving");
+        }
+    }
+    else if (savingAmount > 100) {
+        alert("Put a number bellow 100")
+    }
+    else {
+        alert("Please fill the saving persentage");
+    }
+
 });
